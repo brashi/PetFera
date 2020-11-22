2376a228bc33bc7d7523092fe32b34d645519994
Lucas Moura <lluckymou@gmail.com>
2020-11-22 15:28:55 -0300

    Gitignore petFera e testes de impressão

--- src/funcionarios/tratador.cpp
+++ src/funcionarios/tratador.cpp
@@ -1,5 +1,17 @@
 #include "tratador.hpp"
 
+#include <iomanip>
+#include <iostream>
+
+using std::setfill;
+using std::setw;
+using std::fixed;
+using std::setprecision;
+using std::left;
+using std::right;
+using std::cout;
+using std::endl;
+
 Tratador::Tratador(string nome, string telefone, string email,
                     Uniforme uniforme):
                     Pessoa(nome, telefone, email),
@@ -12,11 +24,14 @@ Uniforme Tratador::getUniforme() const {
 }
 
 ostream& Tratador::printOutDados(ostream& o) const {
-    o << "Nome: " << this->getNome() << endl
-    << "Email: " << this->getEmail() << endl
-    << "Telefone: " << this->getTelefone() << endl
-    << "Uniforme: " 
-    << (this->getUniforme()==Verde ? "Verde" : this->getUniforme()==Azul ? "Azul" : "Vermelho") << endl;
+    int tamanhoColuna = 20, espacamento = 3;
+    int tamanhoTruncado = tamanhoColuna - 3 - espacamento;
+
+    o << setfill(' ') << setw(tamanhoColuna + espacamento) << ((this->getNome().length() > tamanhoTruncado) ? (this->getNome().substr(0, tamanhoTruncado) + "...") : this->getNome())
+    << setfill(' ') << setw(tamanhoColuna + espacamento) << ((this->getEmail().length() > tamanhoTruncado) ? (this->getEmail().substr(0, tamanhoTruncado) + "...") : this->getEmail())
+    << setfill(' ') << setw(tamanhoColuna + espacamento) << ((this->getTelefone().length() > tamanhoTruncado) ? (this->getTelefone().substr(0, tamanhoTruncado) + "...") : this->getTelefone())
+    << setfill(' ') << setw(tamanhoColuna + espacamento) << (this->getUniforme()==Verde ? "Verde" : this->getUniforme()==Azul ? "Azul" : "Vermelho")
+    << endl;
 
     return o;
 }
--- src/funcionarios/veterinario.cpp
+++ src/funcionarios/veterinario.cpp
@@ -1,5 +1,17 @@
 #include "veterinario.hpp"
 
+#include <iomanip>
+#include <iostream>
+
+using std::setfill;
+using std::setw;
+using std::fixed;
+using std::setprecision;
+using std::left;
+using std::right;
+using std::cout;
+using std::endl;
+
 Veterinario::Veterinario(string nome, string telefone, string email,
                     string CRMV):
                     Pessoa(nome, telefone, email),
@@ -12,10 +24,15 @@ string Veterinario::getCRMV() const {
 }
 
 ostream& Veterinario::printOutDados(ostream& o) const {
-    o << "Nome: " << this->getNome() << endl
-    << "Email: " << this->getEmail() << endl
-    << "Telefone: " << this->getTelefone() << endl
-    << "CRMV: " << this->getCRMV() << endl;
+    int tamanhoColuna = 20;
+    int espacamento = 3;
+    int tamanhoTruncado = tamanhoColuna - 3 - espacamento;
+
+    o << setfill(' ') << setw(tamanhoColuna + espacamento) << ((this->getNome().length() > tamanhoTruncado) ? (this->getNome().substr(0, tamanhoTruncado) + "...") : this->getNome())
+    << setfill(' ') << setw(tamanhoColuna + espacamento) << ((this->getEmail().length() > tamanhoTruncado) ? (this->getEmail().substr(0, tamanhoTruncado) + "...") : this->getEmail())
+    << setfill(' ') << setw(tamanhoColuna + espacamento) << ((this->getTelefone().length() > tamanhoTruncado) ? (this->getTelefone().substr(0, tamanhoTruncado) + "...") : this->getTelefone())
+    << setfill(' ') << setw(tamanhoColuna + espacamento) << ((this->getCRMV().length() > tamanhoTruncado) ? (this->getCRMV().substr(0, tamanhoTruncado) + "...") : this->getCRMV())
+    << endl;
 
     return o;
 }
